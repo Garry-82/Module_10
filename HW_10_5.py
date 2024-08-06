224b7d1c0665298e2158747dffec7cb4035c036c
Garry-82 <168720206+Garry-82@users.noreply.github.com>
2024-08-06 13:20:37 +0300

    Add files via upload

---
+++ HW_10_5.py
@@ -0,0 +1,48 @@
+import multiprocessing
+class WarehouseManager():
+#    data = {}
+    def __init__(self):
+        super().__init__()
+#        self.name = name
+#        self.action = action
+#        self.how_mach = how_mach
+        self.data = {}
+    def run(self, requests):
+        with multiprocessing.Pool(4) as pool:
+             pool.map(self.process_request, requests)
+    def process_request(self, request):
+        if request[1] == "receipt":  #  -  пополнение товара на склад
+            if request[0] in self.data.keys():
+                self.data[request[0]] += request[2]
+            if request[0] not in self.data:
+                self.data[request[0]] = request[2]
+
+        if request[1] == "shipment":  #  -  отгрузка товара со склада
+            if request[0] in self.data:
+                self.data[request[0]] -= request[2]
+            if request[0] not in self.data:
+                print(f"Товар {request[0]} отсутстует на складе!! Его отгрузка невозможна!!")
+
+
+if __name__ == '__main__':
+    # Создаем менеджера склада
+    manager = WarehouseManager()
+
+
+    # Множество запросов на изменение данных о складских запасах
+    requests = [
+        ("product1", "receipt", 100),
+        ("product2", "receipt", 150),
+        ("product1", "shipment", 30),
+        ("product3", "receipt", 200),
+        ("product2", "shipment", 50)
+    ]
+
+    # Запускаем обработку запросов
+    manager.run(requests)
+
+    # Выводим обновленные данные о складских запасах
+    print(manager.data)
+
+
+
